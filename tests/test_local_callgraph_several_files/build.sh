1985d47e476d6c99823604fdec704e564887e00c
Hugues Balp <hugues.balp@thalesgroup.com>
2015-07-13 11:43:16 +0200

    Added some commented-out build-commands

--- tests/test_local_callgraph_several_files/build.sh
+++ tests/test_local_callgraph_several_files/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=Debug .
+export CC=`which clang`
+export CXX=`which clang++`
+
+cmake -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=Debug .
+
+#scan-build -v -analyze-headers --use-cc=`which clang`  --use-c++=`which clang++` --use-analyzer=`which callers` -stats -o scan-build-report make
+
+# DumpCallGraph
+#scan-build -v -analyze-headers --use-cc=`which clang`  --use-c++=`which clang++` --use-analyzer=`which clang++` -enable-checker debug.DumpCallGraph -stats -o scan-build-report make
+#scan-build -v -V -enable-checker debug.DumpCallGraph -analyze-headers --use-analyzer=`which clang++` -o scan-build-report make
+scan-build -v -V -enable-checker debug.DumpCallGraph -analyze-headers --use-analyzer=`which clang` -o scan-build-report make
+#scan-build -v -V -enable-checker debug.DumpCallGraph -analyze-headers --use-cc=`which clang`  --use-c++=`which clang++` --use-analyzer=`which clang++` -o scan-build-report make
+
+# ViewCallGraph
+#scan-build -v -analyze-headers --use-cc=`which clang`  --use-c++=`which clang++` --use-analyzer=`which clang++` -enable-checker debug.ViewCallGraph -stats -o scan-build-report make
+

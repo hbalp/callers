788b8553f90ae3af45806102f4077e2ba44a7e85
Hugues Balp <hugues.balp@thalesgroup.com>
2015-07-16 15:01:59 +0200

    Added a generic script to launch callers analysis

---
+++ tests/test_local_callgraph_several_files/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf CMakeFiles
+rm -f mytest
+rm -f compile_commands.json
+rm -f Makefile
+rm -f cmake_install.cmake
+rm -f CMakeCache.txt
+rm -f *.gen.*
+rm -rf scan-build-report
+rm -rf callers-report
+
--- tests/test_local_callgraph_several_files/runtest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-./launch_tests.sh A.cpp
-./launch_tests.sh B.cpp
-./launch_tests.sh test.cpp
-

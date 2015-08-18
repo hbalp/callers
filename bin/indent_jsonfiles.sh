a0e4c88e0e833a2f74356a0a377950da6edf6f03
Hugues Balp <hugues.balp@thalesgroup.com>
2015-08-18 17:39:48 +0200

    Utility to be used for indenting all json files included in a given directory tree.

---
+++ bin/indent_jsonfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# author: Hugues Balp
+# WARNING: We assume here that only one input parameter is present and correspond to a valid directory.
+dir=$1
+
+echo "Try to indent all json files present in directory \"${dir}\""
+
+for json in `find $dir -name "*.json"`
+do
+tmp="${json}.tmp"
+echo "* indent json file: ${json}"
+cp ${json} ${tmp}
+ydump ${tmp} > ${json}
+rm ${tmp}
+done

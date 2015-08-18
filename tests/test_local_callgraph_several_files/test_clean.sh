e7ef4491c9295cbe46e01c3a6b0e6dfeecd3dbf2
Hugues Balp <hugues.balp@thalesgroup.com>
2015-08-18 19:00:59 +0200

    Validate the generation of extcallers

--- tests/test_local_callgraph_several_files/test_clean.sh
+++ tests/test_local_callgraph_several_files/test_clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #set -x
 
+rm -f *.gen.dot
 rm -f *.gen.json
 rm -f *.svg
 rm -rf build
--- tests/test_local_callgraph_several_files/test_start.sh
+++ tests/test_local_callgraph_several_files/test_start.sh
@@ -9,9 +9,14 @@ make
 cd ..
 # generation of file build/defined_symbols.json
 list_defined_symbols.sh `pwd`
+# launch callers analysis
 cd build
 cmake_callers_analysis.sh compile_commands.json all callers-reports 2>&1 | tee analysis.log
 cd ..
+# add extcallers to json files
+add_extcallers.sh .
+indent_jsonfiles.sh .
+# generate callee's tree from main entry point
 function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp
 dot -Tsvg main.fct.callees.gen.dot > main.fct.callees.gen.dot.svg
 inkscape main.fct.callees.gen.dot.svg

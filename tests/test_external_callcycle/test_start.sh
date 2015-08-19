43a71abbc0d82d85d99c7e2edacc2ac5f5c66cc7
Hugues Balp <hugues.balp@thalesgroup.com>
2015-08-19 13:49:53 +0200

    Use option "files" to generate clusters for files in output dot files.

--- tests/test_external_callcycle/test_start.sh
+++ tests/test_external_callcycle/test_start.sh
@@ -18,19 +18,20 @@ add_extcallers.sh .
 indent_jsonfiles.sh .
 
 ## generate callee's tree from main entry point
-function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp
-dot -Tsvg main.fct.callees.gen.dot > main.fct.callees.gen.dot.svg
+#function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp
+function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp files
+#dot -Tsvg main.fct.callees.gen.dot > main.fct.callees.gen.dot.svg
 #inkscape main.fct.callees.gen.dot.svg
 
 ## generate caller's tree from main entry point
-function_callers_to_dot.native callers "main" "int main()" `pwd`/test.cpp
-dot -Tsvg main.fct.callers.gen.dot > main.fct.callers.gen.dot.svg
+#function_callers_to_dot.native callers "main" "int main()" `pwd`/test.cpp
+function_callers_to_dot.native callers "main" "int main()" `pwd`/test.cpp files
+#dot -Tsvg main.fct.callers.gen.dot > main.fct.callers.gen.dot.svg
 #inkscape main.fct.callers.gen.dot.svg
 
 ## generate a call graph from "int A::a()" to "int c()"
 function_callers_to_dot.native c2c "A_a" "int A::a()" `pwd`/A.cpp "c" "int c()" `pwd`/B.cpp
-dot -Tsvg A_a.fct.callees.gen.dot > A_a.fct.callees.gen.dot.svg
-dot -Tsvg A_a.c.c2c.gen.dot > A_a.c.c2c.gen.dot.svg
-dot -Tsvg c.fct.callers.gen.dot > c.fct.callers.gen.dot.svg
-#dot -Tsvg A_a.c.fct.callers.gen.dot > main.fct.callers.gen.dot.svg
+#dot -Tsvg A_a.fct.callees.gen.dot > A_a.fct.callees.gen.dot.svg
+#dot -Tsvg A_a.c.c2c.gen.dot > A_a.c.c2c.gen.dot.svg
+#dot -Tsvg c.fct.callers.gen.dot > c.fct.callers.gen.dot.svg
 #inkscape main.fct.callers.gen.dot.svg

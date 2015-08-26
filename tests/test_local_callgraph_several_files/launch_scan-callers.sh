5621d793227e10cffafbe3fdf98c14c39671128d
Hugues Balp <hugues.balp@thalesgroup.com>
2015-08-26 10:44:30 +0200

    Updated launch script for scan-callers

--- tests/test_local_callgraph_several_files/launch_scan-callers.sh
+++ tests/test_local_callgraph_several_files/launch_scan-callers.sh
@@ -2,20 +2,7 @@
 #set -x
 
 # clean test
-source clean_test.sh
-
-# build the application and get all defined symbols
-mkdir build
-cd build
-cmake ..
-make
-cd ..
-
-# generation of file defined_symbols.json
-list_defined_symbols.sh `pwd`
-rm /tmp/defined_symbols.json
-ln -s `pwd`/defined_symbols.json /tmp/defined_symbols.json
-rm -rf build
+source clean.sh
 
 # launch callers analysis
 mkdir analysis
@@ -31,11 +18,22 @@ fi
 #cmake_callers_analysis.sh compile_commands.json all callers-reports 2>&1 | tee analysis.log
 cd ..
 
+# List generated json files
+list_json_files_in_dirs.native `pwd` .json dir.callers.gen.json
+
+# List all defined symbols in file defined_symbols.json
+list_defined_symbols.native defined_symbols.json test_local_callgraph_several_files dir.callers.gen.json
+read_defined_symbols.native defined_symbols.json file.callers.gen.json
+
+# add extcallees to json files
+source add_extcallees.sh `pwd` defined_symbols.json
+
 # add extcallers to json files
-add_extcallers.sh .
-indent_jsonfiles.sh .
+source add_extcallers.sh .
+source indent_jsonfiles.sh .
 
 # generate callee's tree from main entry point
-function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp
-process_dot_files.sh . analysis/callers
+#function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp
+function_callers_to_dot.native callees "main" "int main()" `pwd`/test.cpp files
+source process_dot_files.sh . analysis/callers
 inkscape analysis/callers/svg/main.fct.callees.gen.dot.svg

a929f89fa56e62ecf84ae78437f18a2818336f24
Hugues Balp <hugues.balp@thalesgroup.com>
2015-09-01 18:27:31 +0200

    Harmonization of absolute paths to avoid duplicating some dot clusters

--- tests/test_local_callgraph_several_files/launch_cmake.sh
+++ tests/test_local_callgraph_several_files/launch_cmake.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #set -x
 
+canonical_pwd="/media/sf_users_3u$PWD"
+
 source "../../bin/common.sh"
 
 #analysis_type=$1
@@ -48,10 +50,10 @@ then
     source indent_jsonfiles.sh .
 
     # generate callee's tree from main entry point
-    source function_callers_to_dot.sh callees "main" "int main()" `pwd`/test.cpp files
+    source function_callers_to_dot.sh callees "main" "int main()" $canonical_pwd/test.cpp files
 
     # generate caller's tree from main entry point
-    source function_callers_to_dot.sh callers "c" "int c()" `pwd`/dirB/B.cpp files
+    source function_callers_to_dot.sh callers "c" "int c()" $canonical_pwd/dirB/B.cpp files
 
     source process_dot_files.sh . analysis/${analysis_type}
 

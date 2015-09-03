6910a559fb2c00ab145edebbf1804773ff7739b8
Hugues Balp <hugues.balp@thalesgroup.com>
2015-09-03 07:14:27 +0200

    Some configuration tuning for the SSO Service Provider analysis

--- bin/launch_link_edition.sh
+++ bin/launch_link_edition.sh
@@ -4,16 +4,16 @@
 common=`which common.sh`
 source $common
 
-# List generated json files
-find . -type f -name "*.gen.json.gz" -exec gunzip {} \;
-list_files_in_dirs `pwd` .file.callers.gen.json dir.callers.gen.json
+# # List generated json files
+# #find . -type f -name "*.gen.json.gz" -exec gunzip {} \;
+# list_files_in_dirs `pwd` .file.callers.gen.json dir.callers.gen.json ".libs:.deps:all:doc:msbuild:m4:build-aux:autom4te.cache:demos:samples:Samples:configs:cxxtestplus-0.3:.git:projects:Projects:ref:tests:config"
 
-# List all defined symbols in file defined_symbols.json
-list_defined_symbols defined_symbols.json `pwd` dir.callers.gen.json
-#read_defined_symbols.native defined_symbols.json file.callers.gen.json
+# # List all defined symbols in file defined_symbols.json
+# list_defined_symbols defined_symbols.json `pwd` dir.callers.gen.json
+# #read_defined_symbols.native defined_symbols.json file.callers.gen.json
 
-# # add extcallees to json files
-# source add_extcallees.sh `pwd` defined_symbols.json
+# add extcallees to json files
+source add_extcallees.sh `pwd` defined_symbols.all.gen.json
 
 # # add extcallers to json files
 # source add_extcallers.sh .

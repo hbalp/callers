398a0f0e940d607415f1820a8d10fba667535ca4
Hugues Balp <hugues.balp@thalesgroup.com>
2015-10-15 11:19:04 +0200

    Added manually "records" definitions to the reference test test_local_callgraph_several_files

---
+++ tests/test_local_callgraph_several_files/dirC/D.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "D.hpp"
+
+int D::c()
+{
+  printf("D::c()\n");
+  return 0;
+}
+
+/* Local Variables: */
+/* compile-command: "clang++ -g  -I /c/ProgramFiles/MinGW/include -o test_local_callgraph_cpp test_local_callgraph.cpp" */
+/* End: */

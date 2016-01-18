9e51886b6572346e851ed355b565c78294ee9585
Hugues Balp <hugues.balp@thalesgroup.com>
2016-01-18 23:18:33 +0100

    Corrected calls between files in the generated .callgraph model

---
+++ utilities.hpp
@@ -0,0 +1,11 @@
+#ifndef _CALLERS_UTILITIES_
+#define _CALLERS_UTILITIES_
+
+// {
+// begin source: stackoverflow.com/questions/7053538/how-do-i-encode-a-string-to-base64-using-only-boost
+std::string decode64(const std::string &val);
+std::string encode64(const std::string &val);
+// end source
+// }
+
+#endif

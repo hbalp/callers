7f0da9b9f947227b95c86f2ee599bff706ca8a27
Hugues Balp <hugues.balp@thalesgroup.com>
2016-04-27 22:08:26 +0200

    Aligned with the C version.

--- tests/test_saml_sign/test_saml_sign.cpp
+++ tests/test_saml_sign/test_saml_sign.cpp
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include "test_saml_sign.h"
 
-
+int division(int a, int b)
+{
+  int div = a / b;
+  return div;
+}
 
 int main()
 {
-  
+  int a, b;
+  // int min = -10;
+  int min = -11;
+  int max = 30;
+  int somme = 0;
+  for(a = min; a < max - 5; a++)
+  {
+    for(b = min + 2; b < max; b++)
+    {
+      somme += division(a, b);
+      b++;
+    }
+  }
+
+  return somme;
 }

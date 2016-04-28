679bf395d96237b58427c3f2219f7820a9a5ce77
Hugues Balp <hugues.balp@thalesgroup.com>
2016-04-28 22:41:51 +0200

    Introduced SAML assertions and xmldsig

--- tests/test_saml_sign/test_saml_sign.c
+++ tests/test_saml_sign/test_saml_sign.c
@@ -1,9 +1,8 @@
 /*
- * gjobread.c : a small test program for gnome jobs XML format
- *
- * See Copyright for the status of this software.
- *
- * Daniel.Veillard@w3.org
+ * test_saml_sign.c: libxml c parsing of openSAML assertions
+ * copyright Thales Communications & Security 2013 to 2016
+ * IST STANCE project
+ * hugues.balp@thalesgroup.com
  */
 
 #include <stdio.h>
@@ -26,10 +25,10 @@
 #define DEBUG(x) printf(x)
 
 /*
- * A person record
+ * A signature record
  * an xmlChar * is really an UTF8 encoded char string (0 terminated)
  */
-typedef struct person {
+typedef struct signature {
     xmlChar *name;
     xmlChar *issueInstant;
     xmlChar *company;
@@ -37,25 +36,25 @@ typedef struct person {
     xmlChar *smail;
     xmlChar *webPage;
     xmlChar *phone;
-} person, *personPtr;
+} signature, *signaturePtr;
 
 /*
  * And the code needed to parse it
  */
-static personPtr
+static signaturePtr
 parseNameID(xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur) {
-    personPtr ret = NULL;
+    signaturePtr ret = NULL;
 
 DEBUG("parseNameID\n");
     /*
      * allocate the struct
      */
-    ret = (personPtr) malloc(sizeof(person));
+    ret = (signaturePtr) malloc(sizeof(signature));
     if (ret == NULL) {
         fprintf(stderr,"out of memory\n");
 	return(NULL);
     }
-    memset(ret, 0, sizeof(person));
+    memset(ret, 0, sizeof(signature));
 
     /* We don't care what the top level element name is */
     /* COMPAT xmlChildrenNode is a macro unifying libxml1 and libxml2 names */
@@ -77,7 +76,7 @@ DEBUG("parseNameID\n");
  * and to print it
  */
 static void
-printNameID(personPtr cur) {
+printNameID(signaturePtr cur) {
     if (cur == NULL) return;
     printf("------ NameID\n");
     if (cur->name) printf("	name: %s\n", cur->name);
@@ -91,34 +90,34 @@ printNameID(personPtr cur) {
 }
 
 /*
- * a Description for a Job
+ * a Description for a Assertion
  */
-typedef struct job {
+typedef struct assertion {
     xmlChar *projectID;
     xmlChar *application;
     xmlChar *category;
-    personPtr contact;
+    signaturePtr contact;
     int nbDevelopers;
-    personPtr developers[100]; /* using dynamic alloc is left as an exercise */
-} job, *jobPtr;
+    signaturePtr developers[100]; /* using dynamic alloc is left as an exercise */
+} assertion, *assertionPtr;
 
 /*
- * And the code needed to parse it
+ * And the code needed to parse the assertion
  */
-static jobPtr
-parseJob(xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur) {
-    jobPtr ret = NULL;
+static assertionPtr
+parseAssertion(xmlDocPtr doc, xmlNsPtr ns, xmlNodePtr cur) {
+    assertionPtr ret = NULL;
 
-DEBUG("parseJob\n");
+DEBUG("parseAssertion\n");
     /*
      * allocate the struct
      */
-    ret = (jobPtr) malloc(sizeof(job));
+    ret = (assertionPtr) malloc(sizeof(assertion));
     if (ret == NULL) {
         fprintf(stderr,"out of memory\n");
 	return(NULL);
     }
-    memset(ret, 0, sizeof(job));
+    memset(ret, 0, sizeof(assertion));
 
     /* We don't care what the top level element name is */
     cur = cur->xmlChildrenNode;
@@ -152,11 +151,11 @@ DEBUG("parseJob\n");
  * and to print it
  */
 static void
-printJob(jobPtr cur) {
+printAssertion(assertionPtr cur) {
     int i;
 
     if (cur == NULL) return;
-    printf("=======  Job\n");
+    printf("=======  Assertion\n");
     if (cur->projectID != NULL) printf("projectID: %s\n", cur->projectID);
     if (cur->application != NULL) printf("application: %s\n", cur->application);
     if (cur->category != NULL) printf("category: %s\n", cur->category);
@@ -168,19 +167,19 @@ printJob(jobPtr cur) {
 }
 
 /*
- * A pool of Gnome Jobs
+ * A pool of Gnome Assertions
  */
-typedef struct gjob {
-    int nbJobs;
-    jobPtr jobs[500]; /* using dynamic alloc is left as an exercise */
-} gJob, *gJobPtr;
+typedef struct gassertion {
+    int nbAssertions;
+    assertionPtr assertions[500]; /* using dynamic alloc is left as an exercise */
+} gAssertion, *gAssertionPtr;
 
-static gJobPtr
-parseGjobFile(char *filename) {
+static gAssertionPtr
+parseGassertionFile(char *filename) {
     xmlDocPtr doc;
-    gJobPtr ret;
-    jobPtr curjob;
-    xmlNsPtr ns;
+    gAssertionPtr ret;
+    assertionPtr curassertion;
+    xmlNsPtr ns_saml, ns_dsig;
     xmlNodePtr cur;
 
 #ifdef LIBXML_SAX1_ENABLED
@@ -206,9 +205,9 @@ parseGjobFile(char *filename) {
 	xmlFreeDoc(doc);
 	return(NULL);
     }
-    ns = xmlSearchNsByHref(doc, cur,
+    ns_saml = xmlSearchNsByHref(doc, cur,
                            (const xmlChar *) "urn:oasis:names:tc:SAML:2.0:assertion");
-    if (ns == NULL) {
+    if (ns_saml == NULL) {
         fprintf(stderr,
 	        "document of the wrong type, OASIS SAML Namespace not found\n");
 	xmlFreeDoc(doc);
@@ -223,18 +222,18 @@ parseGjobFile(char *filename) {
     /*
      * Allocate the structure to be returned.
      */
-    ret = (gJobPtr) malloc(sizeof(gJob));
+    ret = (gAssertionPtr) malloc(sizeof(gAssertion));
     if (ret == NULL) {
         fprintf(stderr,"out of memory\n");
 	xmlFreeDoc(doc);
 	return(NULL);
     }
-    memset(ret, 0, sizeof(gJob));
+    memset(ret, 0, sizeof(gAssertion));
 
     /*
      * Now, walk the tree.
      */
-    /* First level we expect just Jobs */
+    /* First level we expect just Assertions */
     cur = cur->xmlChildrenNode;
     while ( cur && xmlIsBlankNode ( cur ) ) {
 	cur = cur -> next;
@@ -242,10 +241,25 @@ parseGjobFile(char *filename) {
     if ( cur == 0 ) {
 	xmlFreeDoc(doc);
 	free(ret);
-	return ( NULL );
+	return (NULL);
     }
-    if ((xmlStrcmp(cur->name, (const xmlChar *) "Job")) || (cur->ns != ns)) {
-        fprintf(stderr,"document of the wrong type, was '%s', Job expected",
+
+    if (!xmlStrcmp(cur->name, (const xmlChar *) "Signature"))
+    {
+        ns_dsig = xmlSearchNsByHref(doc, cur,
+                                  (const xmlChar *) "http://www.w3.org/2000/09/xmldsig#");
+        if (ns_dsig == NULL) {
+            fprintf(stderr,
+                    "document of the wrong type, xmldsig Namespace not found\n");
+            xmlFreeDoc(doc);
+            return(NULL);
+        }
+    }
+
+    if ((xmlStrcmp(cur->name, (const xmlChar *) "Signature")) &&
+        (xmlStrcmp(cur->name, (const xmlChar *) "Assertion")))
+    {
+        fprintf(stderr,"document of the wrong type, was '%s', Assertion expected",
 		cur->name);
 	fprintf(stderr,"xmlDocDump follows\n");
 #ifdef LIBXML_OUTPUT_ENABLED
@@ -257,16 +271,28 @@ parseGjobFile(char *filename) {
 	return(NULL);
     }
 
-    /* Second level is a list of Job, but be laxist */
+    /* Second level is a list of Assertion, but be laxist */
     /* cur = cur->xmlChildrenNode; */
     /* while (cur != NULL) { */
-        if ((!xmlStrcmp(cur->name, (const xmlChar *) "Job")) &&
-	    (cur->ns == ns)) {
-	    curjob = parseJob(doc, ns, cur);
-	    if (curjob != NULL)
-	        ret->jobs[ret->nbJobs++] = curjob;
-            // if (ret->nbJobs >= 500) break;
+
+        if ((!xmlStrcmp(cur->name, (const xmlChar *) "Signature")) &&
+	    (cur->ns == ns_dsig)) {
+	    curassertion = parseAssertion(doc, ns_dsig, cur);
+	    if (curassertion != NULL)
+	        ret->assertions[ret->nbAssertions++] = curassertion;
+            // if (ret->nbAssertions >= 500) break;
 	}
+
+	cur = cur -> next;
+
+        if ((!xmlStrcmp(cur->name, (const xmlChar *) "Assertion")) &&
+	    (cur->ns == ns_saml)) {
+	    curassertion = parseAssertion(doc, ns_saml, cur);
+	    if (curassertion != NULL)
+	        ret->assertions[ret->nbAssertions++] = curassertion;
+            // if (ret->nbAssertions >= 500) break;
+	}
+
     /*     cur = cur->next; */
     /* } */
 
@@ -274,28 +300,28 @@ parseGjobFile(char *filename) {
 }
 
 static void
-handleGjob(gJobPtr cur) {
+handleGassertion(gAssertionPtr cur) {
     int i;
 
     /*
      * Do whatever you want and free the structure.
      */
-    printf("%d Jobs registered\n", cur->nbJobs);
-    for (i = 0; i < cur->nbJobs; i++) printJob(cur->jobs[i]);
+    printf("%d Assertion(s) registered\n", cur->nbAssertions);
+    for (i = 0; i < cur->nbAssertions; i++) printAssertion(cur->assertions[i]);
 }
 
 int main(int argc, char **argv) {
     int i;
-    gJobPtr cur;
+    gAssertionPtr cur;
 
     /* COMPAT: Do not genrate nodes for formatting spaces */
     LIBXML_TEST_VERSION
     xmlKeepBlanksDefault(0);
 
     for (i = 1; i < argc ; i++) {
-	cur = parseGjobFile(argv[i]);
+	cur = parseGassertionFile(argv[i]);
 	if ( cur )
-	  handleGjob(cur);
+	  handleGassertion(cur);
 	else
 	  fprintf( stderr, "Error parsing file '%s'\n", argv[i]);
 

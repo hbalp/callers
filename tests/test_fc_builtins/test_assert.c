7cf01044d2d2e1c42dcc125f37ac139e3fc2b51c
Hugues Balp <hugues.balp@free.fr>
2016-05-09 19:04:50 +0200

    Commented-out erroneous attempts to use builtin assert.

--- tests/test_fc_builtins/test_assert.c
+++ tests/test_fc_builtins/test_assert.c
@@ -1,18 +1,19 @@
+/* #if 0 */
+/* #define assert Frama_C_assert */
+/* #else */
+/* void assert(int val) */
+/* { */
+/*   if(val == 0) */
+/*     abort(); */
+/* } */
+/* #endif */
 
-#if 1
-#define assert Frama_C_assert
-#else
-void assert(int val)
-{
-  if(val == 0)
-    abort();
-}
-#endif
+#include <assert.h>
 
 int main()
 {
-  //int res = 0;
-  int res = 2;
+  int res = 0;
+  //int res = 2;
   if(res == 0)
   {
     assert(0);
